6745c72c9c6ebb400a92bea31a7356e3488261d1
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-06-18 12:52:43 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -3,15 +3,21 @@ const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
 const schema = require('./graphql/schema');
 const initDB = require('./database');
+const cors = require('@koa/cors');
+
+require('dotenv').config();
 
 initDB();
 
 const app = new Koa();
 
+app.use(cors());
+
 app.use(mount('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true
-})))
+})));
+
 let port = process.env.PORT || 9000;
 
 app.listen(port);
