4e461d586b2fabb289335761c9eec94983b3ed58
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-06-18 12:47:32 +0200

    make it better

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   apps : [{
-    name: 'API',
+    name: 'koa-graphql-mongodb',
     script: 'index.js',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -16,15 +13,4 @@ module.exports = {
       NODE_ENV: 'production'
     }
   }],
-
-  /*deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }*/
 };
